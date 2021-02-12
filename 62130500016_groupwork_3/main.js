b737b21c49ce4a065f3e6d8939f5fa4ebba60c80
guyjiraphat <jiraphat.guy@mail.kmutt.ac.th>
2021-02-12 20:50:10 +0700

    [add] folder

---
+++ 62130500016_groupwork_3/main.js
@@ -0,0 +1,16 @@
+const app = {
+    data() {
+        return {
+            tasks: [{image : './images/guy1.jpg',title: '우리는 서로를 찾아 ',done: false},
+                    {image : './images/guy2.jpg',title: '우리 이야기', done: false},
+                    {image : './images/guy3.jpg',title: '내가 너의 Boss', done: false}
+                ]
+        }
+    },
+    methods: {
+        toggleDone(index){
+            this.tasks[index].done = !this.tasks[index].done
+        }
+    }
+}
+Vue.createApp(app).mount('#app')
